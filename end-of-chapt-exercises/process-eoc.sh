00ee385206c090d9adf910a437bb8db4813f832d
chenemile <chenemile@gmail.com>
2017-07-21 00:35:47 -0400

    Rewrote script to print failed words

--- end-of-chapt-exercises/process-eoc.sh
+++ end-of-chapt-exercises/process-eoc.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 
-echo "Enter filename, e.g. 'Ch2.in': "
-read filename 
+FILES="/home/emily/Documents/Github_Repos/foma.ess/end-of-chapt-exercises"
 
-file="./${filename}"
+# Runs each *.in file through the analyzer and copies the output to a *.out file
+for file in "$FILES"/*.in; do
 
-output=$(echo "$filename" | cut -d\. -f1)
+	output=$(echo "$file" | cut -d"/" -f8 | cut -d\. -f1)
+			 
+	while IFS= read -r line; do
+		sent=$(echo "$line" | cut -f 2- | sed 's/\.$//' | tr '[:upper:]' '[:lower:]')
 
-while IFS= read -r line; do
-	sent=$(echo "$line" | cut -f 2- | sed 's/\.$//' | tr '[:upper:]' '[:lower:]')
+		declare -a 'a=('"$sent"')'
 
-	declare -a 'a=('"$sent"')'
+		printf '%s\n\n' "$line" | sed 's/\t/ /g'
+		for word in "${a[@]}"; do
+			echo "$word" | flookup -w "" ../ess.fomabin
+			printf "\n"
+		done
+		printf '%s\n' '------------------------'
 
-	
-	printf '%s\n\n' "$line" | sed 's/\t/ /g'
-	for word in "${a[@]}"; do
-		echo "$word" | flookup -w "" ../ess.fomabin
-		printf "\n"
-	done
-	printf '%s\n' '------------------------'
+	done <"$file" > "$output".out
+done
 
-done <"$file" > "$output".out
+
+# Prints all words that were not successfully parsed
+for file in "$FILES"/*.out; do
+
+	filename=$(echo "$file" | cut -d"/" -f8 | cut -d\. -f1)
+
+	while IFS= read -r line; do
+		if [[ $line == *"?" ]]; then
+			echo "$filename: $line"
+		fi
+	done <"$file"
+done
