088db6422f062c15808a5b4cc48b7c778e5cec88
chenemile <chenemile@gmail.com>
2017-11-29 14:06:12 -0600

    Updating

--- analyzer_output/jac/jacobson_eoc/process-eoc.sh
+++ analyzer_output/jac/jacobson_eoc/process-eoc.sh
@@ -1,45 +1,36 @@
 #!/bin/bash
 
-FILES="analyzer_output/jac/ungipaghaghlanga"
-
+FILES="/home/emily/Documents/Github_Repos/foma.ess/end-of-chapt-exercises"
 
 # Runs each *.in file through the analyzer and copies the output to a *.out file
 for file in "$FILES"/*.in; do
 
-	output=${file%.in}.out
-
-
-	echo "Processing ${file} to create ${output}..."
-
+	output=$(echo "$file" | cut -d"/" -f8 | cut -d\. -f1)
+			 
 	while IFS= read -r line; do
-		sent=$(echo "$line" | tr -d '[:punct:]' | tr '[:upper:]' '[:lower:]')
+		sent=$(echo "$line" | cut -f 2- | sed 's/\.$//' | tr '[:upper:]' '[:lower:]')
 
 		declare -a 'a=('"$sent"')'
 
 		printf '%s\n\n' "$line" | sed 's/\t/ /g'
 		for word in "${a[@]}"; do
-			echo "$word" | flookup -w "" ./ess.fomabin
+			echo "$word" | flookup -w "" ../ess.fomabin
 			printf "\n"
 		done
 		printf '%s\n' '------------------------'
 
-	done <"$file" > "$output"
+	done <"$file" > "$output".out
 done
 
 
 # Prints all words that were not successfully parsed
 for file in "$FILES"/*.out; do
 
-	filename=${file%.in}
+	filename=$(echo "$file" | cut -d"/" -f8 | cut -d\. -f1)
 
 	while IFS= read -r line; do
 		if [[ $line == *"?" ]]; then
 			echo "$filename: $line"
 		fi
-	done <"$file" > "$filename".errors 
+	done <"$file"
 done
-
-
-
-
-#######
