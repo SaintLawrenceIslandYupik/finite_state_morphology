54548e81d854456c8e9a13f993453f8e5d369198
chenemile <chenemile@gmail.com>
2020-01-15 22:57:55 -0800

    editing script to annotate suffixes

---
+++ make-lexc-files/annotate_suffixes.py
@@ -0,0 +1,171 @@
+'''
+:author:      Hayley Park
+:modified by: Emily Chen
+:date:        2020
+
+usage: python annotate_suffixes.py -s suffix_lists/intrg.txt -r Noun
+
+'''
+import argparse
+import csv
+import glob
+import re 
+from collections import OrderedDict
+
+from classify_roots import idx_to_roman_numeral
+
+
+ # Jacobson (2001) Notation <-> foma v.2 Notation
+symbol_conversion = OrderedDict()
+symbol_conversion["~sf"] = "%{.sf.%}"
+symbol_conversion["~f"]  = "%{.f.%}"
+symbol_conversion["~"]   = "%{.f.%}%{.sf.%}"
+symbol_conversion["@"]   = "%{.at.%}"
+symbol_conversion[":"]   = "%{.c.%}"
+symbol_conversion["–"]   = "%{.m.%}"
+symbol_conversion["-w"]  = "%{.w.%}"
+symbol_conversion["(e)"] = "%{E%}"
+symbol_conversion["(g)"] = "%{G%}"
+symbol_conversion["+"]   = ""
+
+rules_for_noun_roots = [ # Noun Class I: end in -a, -i, -u
+                         {"%{E%}":"", "(ng)":"ng", "(te)":""},
+                         # Noun Class II: end in -g, -w, -ghw
+                         {"%{E%}":"e", "(ng)":"", "(te)":"te"},
+                         # Noun Class III: end in weak -gh
+                         {"%{E%}":"", "(ng)":"", "(te)":"te", "%{.m.%}":"%{.m.%}", "%{.w.%}":"%{.w.%}", "%{.c.%}":"%{.c.%}"},
+                         # Noun Class IV: end in marked strong -gh (*)
+                         {"%{E%}":"%{E%}", "(ng)":"", "(te)":"te", "%{.m.%}":"%{.m.%}", "%{.c.%}":"%{.c.%}"},
+                         # Noun Class V: end in strong -gh
+                         {"%{E%}":"e", "(ng)":"", "(te)":"te", "%{.m.%}":"%{.m.%}"},
+                         # Noun Class VI: end in -te 
+                         {"%{E%}":"", "(ng)":"ng", "(te)":"", "%{.f.%}":"%{.f.%}"},
+                         # Noun Class VII: have semi-final -e
+                         {"%{E%}":"e", "(ng)":"", "(te)":"te", "%{.sf.%}":"%{.sf.%}", "%{.m.%}":"%{.m.%}"},
+                         # Noun Class VIII: end in -e that can be dropped but not hopped
+                         {"%{E%}":"", "(ng)":"ng", "(te)":"", "%{.f.%}":"%{.f.%}"},
+                         # Noun Class IX: end in -e that can be dropped and hopped
+                         {"%{E%}":"", "(ng)":"ng", "(te)":"", "%{.f.%}":"%{.f.%}"}
+                       ]
+
+rules_for_verb_roots = [ # Verb Class I: end in -a, -i, -u
+                         {"(g/t)": "%{G%}", "%{G%}":"%{G%}", "(t)":"", "(te)":""},
+                         # Verb Class II: end in -e
+                         {"(g/t)": "", "(t)":"", "(te)":"", "%{.f.%}": "%{.f.%}", "%{G%}":"%{G%}"},
+                         # Verb Class III: end in -g, -w, -ghw
+                         {"(g/t)": "t", "(t)":"t", "(te)":"te", "%{E%}":"e", "%{.m.%}": "%{.m.%}"},
+                         # Verb Class IV: end in -agh, -igh, -ugh
+                         {"(g/t)": "t", "(t)":"t", "(te)":"te",  "%{E%}":"e", "%{.m.%}": "%{.m.%}"},
+                         # Verb Class V: end in -te
+                         {"(g/t)": "", "(t)":"", "(te)":"", "%{.f.%}": "%{.f.%}", "%{.at.%}": "%{.at.%}"},
+                         # Verb Class VI: end in special -te
+                         {"(g/t)": "", "(t)":"", "(te)":"", "%{.f.%}": "%{.f.%}", "%{.at.%}": "%{.at.%}"},
+                         # Verb Class VII: have semi-final -e
+                         {"(g/t)": "t", "(t)":"t", "(te)":"te", "%{E%}":"e", "%{.sf.%}%{G%}": "%{.sf.%}%{G%}", "%{.sf.%}": "%{.sf.%}", "%{.m.%}": "%{.m.%}"},
+                       ]
+
+
+def replace_all(my_text, my_dict):
+    '''
+    :param my_text: string to undergo replacement 
+    :type my_text: str
+    :param my_dict: associates original text 'i' with
+                    its replacement text 'j' 
+    :type my_dict: dict
+
+    Replaces all instances of 'i' in my_text with 'j'.
+    '''
+    for i, j in my_dict.items():
+        my_text = my_text.replace(i, j)
+    return my_text
+
+
+def annotate_suffixes(my_suffixes, my_root_type):
+    '''
+    :param my_suffixes: path to file containing suffixes
+    :type my_suffixes: str
+    :param my_root_type: "Noun" or "Verb"
+    :type my_root_type: str
+
+    :return: A dictionary where each key is an integer
+             representing an inflection class, and each
+             value is a string containing all of the
+             suffixes annotated for a particular class,
+             and their definitions.
+
+    '''
+    class2suffixes = {}
+
+    if my_root_type == "Noun":
+        rules = rules_for_noun_roots
+        n_class = len(rules)
+        
+    elif my_root_type == "Verb":
+        rules = rules_for_verb_roots
+        n_class = len(rules)
+
+    else:
+        raise ValueError('root_type needs to be either "Noun" or "Verb"')
+
+    with open(my_suffixes, 'r') as suffix_input:
+        suffixes_jacobson_notation = suffix_input.read()
+
+    class2suffixes[0] = suffixes_jacobson_notation
+
+    for i in range(1, n_class+1):
+
+        # convert Jacobson (2001) Notation to v.2 Notation
+        suffixes_v2_notation = replace_all(suffixes_jacobson_notation, symbol_conversion)
+
+        # rewrite certain symbols as required for a given class of roots
+        suffixes_class_specific = replace_all(suffixes_v2_notation, rules[i-1])
+
+        for symbol in symbol_conversion.values():
+            # remove any symbols that are not necessary for the given class of roots 
+            if symbol not in rules[i-1]:
+                final_annotated_suffixes = suffixes_class_specific.replace(symbol, "")
+                suffixes_class_specific = final_annotated_suffixes
+    
+        class2suffixes[i] = suffixes_class_specific
+
+    return class2suffixes
+
+
+def print_suffixes(inflClass, jacobson_notation_suffixes, class_specific_suffixes, rootType):
+    romanNumeral = idx_to_roman_numeral(inflClass)
+
+    print("-" * 22 + "-" * len(romanNumeral)) 
+    print(rootType + " Inflection Class " + romanNumeral)
+    print("-" * 22 + "-" * len(romanNumeral)) 
+
+    jacobsonSuffixes = jacobson_notation_suffixes.split("\n")
+    annotatedSuffixes = class_specific_suffixes.split("\n")
+    
+    for i, suffixWithDefinition in enumerate(annotatedSuffixes):
+        if suffixWithDefinition:
+            suffix, definition = suffixWithDefinition.split(",")
+            # ^^^ CLASSIFY SUFFIX
+            jacobsonSuffix = re.sub('-|$', '', jacobsonSuffixes[i].split(",")[0])
+
+            print(jacobsonSuffix + ":^" + suffix)
+            # ^^^ CALCULATE PADDING BASED OFF THE ABOVE STRING
+    print()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--suffixes', '-s', type=str, help = 'path to file containing derivational suffixes')
+    parser.add_argument('--root_type', '-r', type=str, help = 'type of root: either Noun or Verb')
+    args = parser.parse_args()
+
+    # annotate each suffix for each inflection class
+    class2suffixes = annotate_suffixes(args.suffixes, args.root_type)
+
+    for inflectionClass in class2suffixes.keys():
+        # class2suffixes[0] lists the suffixes annotated in Jacobson (2001) Notation
+        if inflectionClass > 0:
+            print_suffixes(inflectionClass, class2suffixes[0], class2suffixes[inflectionClass], args.root_type)
+
+
+if __name__ == "__main__":
+    main()
---
+++ make-lexc-files/classify_roots.py
@@ -0,0 +1,304 @@
+'''
+:author: Emily Chen
+:date:   2019
+
+Given a root list in .csv format, classifies each root
+into its respective inflection class.
+
+Eventually, this script should automatically generate
+the lexc files from the completed Yupik database.
+
+NOTE: There are several problem children for this script
+      when it comes to identifying part of speech,
+      especially if "particle" or "?" appears in the
+      definition of a noun root or verb root (e.g. tefli-).
+
+Usage: python3 classify.py [dirname]
+'''
+import argparse
+import csv
+import glob
+from operator import itemgetter
+
+from methods_for_classify import classify_verb_root, \
+                                 convert_to_base_form, classify_noun_root
+
+
+def print_inflection_classes(inflType, idx2InflClass):
+    '''
+    :param inflType: "Noun" or "Verb"
+    :type inflType: str
+    :param idx2InflClass: dictionary where each key is an
+                          inflection class and its value
+                          is a list of roots that belong to
+                          that inflection class
+    :type idx2InflClass: dict
+
+    Janky print function to print each root in each
+    inflection class in lexc-compatible format.
+
+    '''
+    for i in range(1, len(idx2InflClass)):
+        if idx2InflClass[i]:
+
+            romanNumeral = idx_to_roman_numeral(i)
+
+            print("-" * 22 + "-" * len(romanNumeral))
+            print(inflType + " Inflection Class " + romanNumeral)
+            print("-" * 22 + "-" * len(romanNumeral))
+
+            # get length of longest root in inflection class
+            # in order to properly calculate padding
+            maxLength = get_max_length(inflType, i, idx2InflClass[i])
+
+            # class of noun roots that end in -g, -w, -ghw
+            if inflType == "Noun" and i == 2:
+                for root, definition in idx2InflClass[i]:
+                    # if noun root ends in -w, add %{k%} -->  kiiw:kii%{k%}w
+                    if root[-1] == "w" and ''.join(root[-3:-1]) != "gh":
+                        padding = maxLength - (len(root) * 2 + 6) + 2
+                        print(root + ":" + root[:-1] + "%{k%}w" + " " * padding + \
+                              inflType + "Suffix" + romanNumeral + "; ! " + definition)
+                    else:
+                        padding = maxLength - len(root) + 2
+                        print(root + " " * padding + inflType + "Suffix" + romanNumeral + \
+                              "; ! " + definition)
+
+            # class of noun roots that end in weak -gh
+            #   add %{g%} --> nasaperagh:nasapera%{g%}h
+            elif inflType == "Noun" and i == 3:
+                for root, definition in idx2InflClass[i]:
+                    padding = maxLength - (len(root) * 2 + 5) + 2
+                    print(root + ":" + root[:-2] + "%{g%}h" + " " * padding + \
+                          inflType + "Suffix" + romanNumeral + "; ! " + definition)
+
+            # class of noun roots that end in marked strong -gh (*)
+            #   remove * at the end of the root, e.g. afsengagh*  --> afsengagh
+            elif inflType == "Noun" and i == 4:
+                for root, definition in idx2InflClass[i]:
+                    padding = maxLength - len(root) + 2
+                    print(root[:-1] + " " * padding + inflType + "Suffix" + romanNumeral + \
+                          "; ! " + definition)
+
+            # class of roots that end in -te
+            #   if root ends in -te, add %{t%} --> riigte:riig%{t%}e
+            elif (inflType == "Noun" and i == 6 or
+                  inflType == "Verb" and i == 5):
+                print()
+                print("[ WARNING: check for roots that actually end in -ta ]")
+                print()
+                for root, definition in idx2InflClass[i]:
+                    padding = maxLength - (len(root) * 2 + 5) + 2
+                    print(root + ":" + root[:-2] + "%{t%}e" + " " * padding + \
+                          inflType + "Suffix" + romanNumeral + "; ! " + definition)
+
+            else:
+                for root, definition in idx2InflClass[i]:
+                    padding = maxLength - len(root) + 2
+                    print(root + " " * padding + inflType + "Suffix" + romanNumeral + \
+                          "; ! " + definition)
+            print()
+
+    # print roots that could not be classified, usually those with non-traditional endings
+    if idx2InflClass[0]:
+        maxLength = get_max_length("None", 0, idx2InflClass[0])
+
+        print("-------------------------------------------")
+        print(inflType + " Roots (?) that Could Not Be Classified")
+        print("-------------------------------------------")
+        for root, definition in idx2InflClass[0]:
+            padding = maxLength - len(root) + 2
+            print(root + " " * padding + definition)
+    print()
+
+
+def idx_to_roman_numeral(idx):
+    '''
+    :param idx: index of inflection class
+    :type idx: int
+
+    :return: str
+
+    Converts the given index to its
+    Roman numeral counterpart.
+
+    '''
+    switcher = { 1: "I"   ,
+                 2: "II"  ,
+                 3: "III" ,
+                 4: "IV"  ,
+                 5: "V"   ,
+                 6: "VI"  ,
+                 7: "VII" ,
+                 8: "VIII",
+                 9: "IX"  ,
+               }
+    return switcher.get(idx, "nothing...")
+
+
+def get_max_length(inflType, classIdx, inflClass):
+    '''
+    :param inflType: "Noun" or "Verb"
+    :type inflType: str
+    :param classIdx: inflection class index
+    :type classIdx: int
+    :param inflClass: the inflection class where each
+                      element is a tuple: (root, definition)
+    :type inflClass: list of tuples
+
+    Calculates the length of the longest root in the inflection
+    class or the length of the longest "altered" root in the
+    inflection class (takes foma-style symbols into account,
+    i.e. %{k%}, %{g%}, %{t%})
+
+    '''
+    maxLength = len((sorted(inflClass, key=lambda x: len(x[0]), reverse=True)[0])[0])
+    alteredMaxLength = 0
+
+    # get length of longest -w root with %{k%} inserted
+    #   e.g. kii%{k%}w
+    if inflType == "Noun" and classIdx == 2:
+        for pair in inflClass:
+            root = pair[0]
+            if root[-1] == "w":
+                alteredLength = len(root) * 2 + 6  # adding :, %, {, k, %, }
+                if alteredLength > alteredMaxLength:
+
+                    alteredMaxLength = alteredLength
+
+        if alteredMaxLength > maxLength:
+            maxLength = alteredMaxLength
+
+    # get length of longest weak -gh root with %{g%} inserted
+    #                       -te root with %{t%} inserted
+    #   e.g. nasapera%{g%}h
+    #        riig%{t%}e
+    elif (inflType == "Noun" and classIdx == 3 or
+          inflType == "Noun" and classIdx == 6 or
+          inflType == "Verb" and classIdx == 5):
+        maxLength = maxLength * 2 + 5 # adding :, %, {, %, }
+
+    return maxLength
+
+
+def print_nonroots(nonRootType, nonRootList):
+    '''
+    :param nonRootType: "Particles" or "Question Words"
+    :type nonRootType: str
+    :param nonRootList: list of non-roots
+    :type nonRootList: list of tuples
+
+    Janky print function to print each non-root (particle
+    or question word) in lexc-compatible format.
+
+    '''
+    print("-" * len(nonRootType))
+    print(nonRootType)
+    print("-" * len(nonRootType))
+
+    # get length of longest noun-root
+    maxLength = len((sorted(nonRootList, key=lambda x: len(x[0]), reverse=True)[0])[0])
+
+    for pair in nonRootList:
+        nonRoot = pair[0]
+        definition = pair[1]
+
+        padding = maxLength - len(nonRoot) + 2
+        print(nonRoot + " " * padding + "#; ! " + definition)
+
+    print()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('dirname', help = 'name of directory containing word lists')
+    args = parser.parse_args()
+
+    # initialize verbal inflection classes
+    idx2VerbClass = { 0 : [],  # cannot be classified
+                      1 : [],  # ends in -a, -i, -u
+                      2 : [],  # ends in -e
+                      3 : [],  # ends in -g, -w, -ghw
+                      4 : [],  # ends in -agh, -igh, -ugh,
+                               #   -egh if -e cannot drop/hop
+                      5 : [],  # ends in -te
+                      6 : [],  # ends in special -te
+                      7 : [],  # ends in semi-final -e
+                    }
+
+    # initialize nominal inflection classes
+    idx2NounClass = { 0 : [],  # cannot be classified
+                      1 : [],  # ends in -a, -i, -u
+                      2 : [],  # ends in -g, -w, -ghw
+                      3 : [],  # ends in weak -gh
+                      4 : [],  # ends in marked strong -gh
+                      5 : [],  # ends in strong -gh
+                      6 : [],  # ends in -te
+                      7 : [],  # ends in semi-final -e
+                      8 : [],  # ends in final -e that
+                               # can drop and hop
+                      9 : []   # ends in final -e that
+                               # can drop but not hop
+                    }
+
+    # initialize list of particles
+    particles = []
+
+    # initialize list of question words
+    questionWords = []
+
+    # initialize list of postural roots
+    posturalRoots = []
+
+    # initialize list of emotional roots 
+    emotionalRoots = []
+
+    for filename in glob.glob(args.dirname + "/*"):
+        with open(filename, 'r') as csvfile:
+            reader = csv.reader(csvfile, delimiter = ',')
+
+            for line in reader:
+                if "particle" in line[1]:
+                    particles.append((line[0], line[1]))
+
+                elif "?" in line[1]:
+                    questionWords.append((line[0], line[1]))
+
+                elif "postural root" in line[1]:
+                    posturalRoots.append((line[0], line[1]))
+
+                elif "emotional root" in line[1]:
+                    emotionalRoots.append((line[0], line[1]))
+
+                else: 
+                    root = line[0]
+
+                    # verb roots are annotated with a hyphen at the end
+                    if root[-1] == "-":
+                        verbRoot = root[:-1]
+                        classIdx = classify_verb_root(verbRoot)
+                        idx2VerbClass[classIdx].append((verbRoot, line[1]))
+
+                    else:
+                        nounRoot = convert_to_base_form(root)
+                        classIdx = classify_noun_root(nounRoot)
+                        idx2NounClass[classIdx].append((nounRoot, line[1]))
+
+    print_inflection_classes("Verb", idx2VerbClass)
+    print_inflection_classes("Noun", idx2NounClass)
+
+    if particles:
+        print_nonroots("Particles", particles)
+
+    if questionWords:
+        print_nonroots("Question Words", questionWords)
+
+    if posturalRoots:
+        print_nonroots("Postural Roots", posturalRoots)
+
+    if emotionalRoots:
+        print_nonroots("Emotional Roots", emotionalRoots)
+
+
+if __name__ == "__main__":
+    main()
